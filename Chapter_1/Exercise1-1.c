9d38d1c71801aeef12f7bdcfd9708773263d3dd6
Rophpad <rophenp@gmail.com>
2023-10-08 12:59:40 +0100

    A program that prints "hello, world" on your ystem

---
+++ Chapter_1/Exercise1-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Function that print "hello, world" on your system
+ *
+ * Return: 0 in case of success
+ */
+int main(void)
+{
+	printf("hello, world\n")
+	return (0);
+}
